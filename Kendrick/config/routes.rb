39e5876c443d3098318a78fca7df7545f7a3f89c
Ali Yeysides <ali.yeysides@gmail.com>
2014-11-20 18:16:06 -0500

    working on routes and user controller

--- Kendrick/config/routes.rb
+++ Kendrick/config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :users
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get 'signup' => 'auth#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
