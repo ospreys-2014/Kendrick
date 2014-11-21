afd6bc5d03fa8fa16f14521a198c03018f98412f
Ali Yeysides <ali.yeysides@gmail.com>
2014-11-21 11:42:21 -0500

    fix signup feature rspec

--- Kendrick/spec/features/signing_up_spec.rb
+++ Kendrick/spec/features/signing_up_spec.rb
@@ -5,9 +5,9 @@ feature 'Signing up' do
   scenario 'Successful sign up' do
    visit '/'
    click_link 'Sign up'
-   fill_in "Email", with: "user@example.com"
-   fill_in "Password", with: "password"
-   fill_in "Password confirmation", with: "password"
+   fill_in "username", with: "Jobin"
+   fill_in "password", with: "password"
+   fill_in "password confirmation", with: "password"
    click_button "Sign up"
    expect(page).to have_content("You have signed up successfully.")
   end
