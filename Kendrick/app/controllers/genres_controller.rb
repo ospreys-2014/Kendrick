80575de87371174d13c520cddb80368bf14ddd06
James Hwang <jhwang90@gmail.com>
2014-11-21 15:47:01 -0500

    Set up basic bootstrap template

--- Kendrick/app/controllers/genres_controller.rb
+++ Kendrick/app/controllers/genres_controller.rb
@@ -2,13 +2,12 @@ class GenresController < ApplicationController
 
   def index
     @genres = Genre.all
-    render layout: false
   end
 
   def show
     genre = Genre.find(params[:id])
     @users = genre.artists
-    render 'show_genre', layout: false
+    render 'show_genre'
   end
 
 end
