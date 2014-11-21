ac072aaa165cbedf54b1f7af1f9e6fa0a7d4dfe1
Ali Yeysides <ali.yeysides@gmail.com>
2014-11-21 11:36:42 -0500

    add private method for user params, fix user create method in user controller

--- Kendrick/app/controllers/users_controller.rb
+++ Kendrick/app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
 
     if @user.save
       flash[:notice] = "You have signed up successfully."
@@ -17,6 +17,12 @@ class UsersController < ApplicationController
     else
       render :new
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
   end
 
 end
