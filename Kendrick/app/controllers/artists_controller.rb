3e2c43728247668daae2f90c6748ab694afeeb09
James Hwang <jhwang90@gmail.com>
2014-11-20 21:16:29 -0500

    Deletes artists controller and artists view because we are using USERS only

--- Kendrick/app/controllers/artists_controller.rb
+++
@@ -1,8 +0,0 @@
-class ArtistsController < ApplicationController
-
-  def show
-    @artists = Artist.all
-    render partial: "index.html.erb", locals: {artists: @artists}
-  end
-
-end
--- Kendrick/app/controllers/genres_controller.rb
+++ Kendrick/app/controllers/genres_controller.rb
@@ -5,4 +5,10 @@ class GenresController < ApplicationController
     render partial: 'index.html', locals: {genres: @genres}
   end
 
+  def show
+    genre = Genre.find(params[:id])
+    @users = User.where(genre)
+    render partial: 'show_genre.html', locals: {users: @users}
+  end
+
 end
