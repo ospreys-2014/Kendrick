963b20350227d7ae9edf1eb744242e471b0df5aa
Steph Reaves <null>
2014-11-20 22:14:58 -0500

    added review form and create route

---
+++ Kendrick/app/controllers/reviews_controller.rb
@@ -0,0 +1,21 @@
+class ReviewsController < ApplicationController
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def new
+    @review = Review.new(params[:review])
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.save
+    redirect_to @review
+  end
+
+  private
+    def review_params
+      params.require(:review).permit(:body)
+    end
+end
