6a15035163486fab4aaaf184b568c1cada8a6bf3
unknownbreaker <rob.yang@gmail.com>
2014-11-22 15:18:23 -0500

    add comment model and migration

---
+++ app/models/comment.rb
@@ -0,0 +1,10 @@
+class Review < ActiveRecord::Base
+  
+  validates   :body, presence: true
+  validates   :commenter, presence: true
+  validates   :review, presence: true
+  
+  belongs_to  :commenter, class_name: "User"
+  belongs_to  :review
+
+end
