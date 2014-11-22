4de14c97fb3319468cf88dd51deabe8afbc83a3c
unknownbreaker <rob.yang@gmail.com>
2014-11-22 18:21:29 -0500

    assocations in regard to receiving and giving ratings; added comments to clarify the meaning behind the associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,10 +13,21 @@ class User < ActiveRecord::Base
   validates_format_of :email_address, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   validates :genres, presence: true, if: :artist
 
-  has_many  :ratings
+  # ratings_as_user means that you are the user giving ratings 
+  # on reviews, comments, or artists
+  has_many  :ratings_as_user, class_name: "User"
 
+  # ratings_as_artist means that you are the artist who
+  # is receiving many ratings (like you are a review or comment)
+  has_many  :ratings_as_artist, class_name: "User"
+
+  # reviews_as_reviewer means you are giving a review as a user
   has_many  :reviews_as_reviewer, class_name: "Review", foreign_key: :reviewer_id
+
+  # reviews_as_artist means you are receiving reviews
+  # as the artist being reviewed about
   has_many  :reviews_as_artist, class_name: "Review", foreign_key: :artist_id
+  
   has_many  :comments, foreign_key: :commenter_id
 
   has_many  :artist_genres, foreign_key: :artist_id
