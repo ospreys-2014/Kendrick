f5753149b5ff3dcf3e7ed973dc377cf1278ad5c4
Ali Yeysides <ali.yeysides@gmail.com>
2014-11-22 16:02:05 -0500

    change redirect after login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:signin][:password])
       session[:user_id] = user.id
       flash[:notice] = "Signed in successfully."
-      redirect_to root_url
+      redirect_to genres_path
     else
       flash[:error] = "Sorry."
       render :new
