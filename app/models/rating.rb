6a91444738abcbb430d1e1bd3c615fc00e2e9076
unknownbreaker <rob.yang@gmail.com>
2014-11-22 18:03:22 -0500

    rating model validates the numeric rating provided to it

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,5 +1,12 @@
 class Rating < ActiveRecord::Base
 
+  validates_numericality_of :rating, 
+                            allow_nil: true,
+                            allow_blank: true,
+                            only_integer: true, 
+                            greater_than_or_equal_to: 1,
+                            less_than_or_equal_to: 5
+                            
   belongs_to  :user
   belongs_to  :artist, class_name: "User"
   belongs_to  :ratingable, polymorphic: true
