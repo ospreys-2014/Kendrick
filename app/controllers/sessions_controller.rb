af557b0f5f84c9dbb15e931b0f1db11d7b74713b
Ali Yeysides <ali.yeysides@gmail.com>
2014-11-22 15:55:46 -0500

    login works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,15 +11,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.where(username: params[:login][:username]).first
+    user = User.where(username: params[:signin][:username]).first
 
-    if user && user.authenticate(params[:login][:password])
+    if user && user.authenticate(params[:signin][:password])
       session[:user_id] = user.id
       flash[:notice] = "Signed in successfully."
       redirect_to root_url
     else
       flash[:error] = "Sorry."
-      render :login
+      render :new
     end
   end
 end
