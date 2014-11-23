d0a3a0e03090c00a216b63bddc04e58fef900c7d
Ali Yeysides <ali.yeysides@gmail.com>
2014-11-23 16:08:09 -0500

    fix signin test

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -3,12 +3,13 @@ require 'spec_helper'
 feature "signing in." do
 
   scenario 'Signing in via form' do
-    user = User.where(username: "ali", password: "1")
+    user = User.create(username: "jajajaj", password: "1", first_name: "ali", last_name: "yeysides", email_address: "123@gmail.com")
     visit '/'
-    click_link 'Login'
-    fill_in 'Username', with: "ali"
-    fill_in 'Password', with: "1"
-    click_button "Sign in"
+    click_link('signin-link')
+    fill_in('Username', with: "jajajaj")
+    fill_in('Password', with: "1")
+    click_button("Sign in")
+    save_and_open_page
     expect(page).to have_content("Signed in successfully.")
   end
 end
