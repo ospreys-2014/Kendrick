ddbf638772ab40317c6a141d870af5381983cc68
Ali Yeysides <ali.yeysides@gmail.com>
2014-11-23 16:11:34 -0500

    add signin test for incorrect info

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -9,7 +9,17 @@ feature "signing in." do
     fill_in('Username', with: "jajajaj")
     fill_in('Password', with: "1")
     click_button("Sign in")
-    save_and_open_page
     expect(page).to have_content("Signed in successfully.")
   end
+
+  scenario 'Signing in via form and entering incorrect info spits an error' do
+    user = User.create(username: "jajajaj", password: "1", first_name: "ali", last_name: "yeysides", email_address: "123@gmail.com")
+    visit '/'
+    click_link('signin-link')
+    fill_in('Username', with: "adssada")
+    fill_in('Password', with: "32323")
+    click_button("Sign in")
+    expect(page).to have_content("Sorry, those credentials are incorrect.")
+  end
+
 end
