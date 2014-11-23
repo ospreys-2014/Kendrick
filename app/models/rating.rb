058ce9574601341b8ea0837abc777a4337e437c6
unknownbreaker <rob.yang@gmail.com>
2014-11-23 17:03:04 -0500

    moved validation up to be kept together for readability

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -6,9 +6,9 @@ class Rating < ActiveRecord::Base
                             only_integer: true, 
                             greater_than_or_equal_to: 1,
                             less_than_or_equal_to: 5
+  validates_with  ArtistValidator
                             
   belongs_to  :user
   belongs_to  :artist, class_name: "User"
   belongs_to  :ratingable, polymorphic: true
-  validates_with  ArtistValidator
 end
