5b8c037c60c757e05d0d4ab68a4c81cc1d3aa702
Ali Yeysides <ali.yeysides@gmail.com>
2014-11-23 16:32:23 -0500

    add test for post signup to see if user can see genres

--- spec/features/signing_up_spec.rb
+++ spec/features/signing_up_spec.rb
@@ -14,4 +14,19 @@ feature 'Signing up' do
    click_button "Sign up"
    expect(page).to have_content("You have signed up successfully.")
   end
+
+  scenario 'Can see genres_path post signup' do
+   visit '/'
+   click_link 'SignUp'
+   fill_in("Username", with: "Jobin")
+   fill_in("First name", with: "jobin")
+   fill_in("Last name", with: "toby")
+   fill_in("Password", with: "password")
+   fill_in("Password confirmation", with: "password")
+   fill_in("Email address", with: "123@gmail.com")
+   click_button "Sign up"
+   save_and_open_page
+   expect(page.current_path).to eq genres_path
+  end
+
 end
