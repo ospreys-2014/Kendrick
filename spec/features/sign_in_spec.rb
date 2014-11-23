b3ab76f370dd2e68273c2eed2a4f6faa0eb875fd
Ali Yeysides <ali.yeysides@gmail.com>
2014-11-23 13:54:31 -0500

    fixed sign in spec

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -8,7 +8,6 @@ feature "signing in." do
     click_link 'Login'
     fill_in 'Username', with: "ali"
     fill_in 'Password', with: "1"
-    save_and_open_page
     click_button "Sign in"
     expect(page).to have_content("Signed in successfully.")
   end
