c894c3033f972fb0988d2c582eb13b0831a8cf80
jhwang90 <jhwang90@gmail.com>
2014-11-24 00:42:29 -0500

    Adds ratings controller #create

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -12,7 +12,16 @@ class RatingsController < ApplicationController
   end
 
   def create
-
+    @rating = Rating.new(rating_params)
+    @rating.artist = User.find(params[:rating][:artist_id])
+    @rating.reviewer = User.find(params[:rating][:reviewer_id])
+    @rating.save
+    redirect_to(:back)
   end
+
+  private
+    def rating_params
+      params.require(:rating).permit(:helpful)
+    end
 
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,6 +1,7 @@
 class Rating < ActiveRecord::Base
 
   validates_with  ArtistValidator
+  # validates :helpful, presence: true
 
   belongs_to  :user
   belongs_to  :artist, class_name: "User"
