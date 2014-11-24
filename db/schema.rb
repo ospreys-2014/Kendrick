0b5688f408fdd386b15628eaa0dc4efd0b48b812
unknownbreaker <rob.yang@gmail.com>
2014-11-24 01:06:08 -0500

    incorporated updates from rebase

--- db/schema.rb
+++ db/schema.rb
@@ -33,7 +33,6 @@ ActiveRecord::Schema.define(version: 20141124040936) do
 
   create_table "genres", force: true do |t|
     t.string   "name"
-    t.string   "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
