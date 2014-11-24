1c2993ff199400be914300e8d284dfa171adb0a8
unknownbreaker <rob.yang@gmail.com>
2014-11-23 21:16:06 -0500

    user controller to display average rating for the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @current_user = User.find(session[:user_id])
     @rating = Rating.new
+    @average_rating = Rating.average_rating_for(@user)
   end
 
   def new
