d46d54466a7de8790fea393747311de4c2a2c4e5
Ali Yeysides <ali.yeysides@gmail.com>
2014-11-23 19:14:16 -0500

    working with this gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,9 @@ class User < ActiveRecord::Base
   validates :email_address, presence: true
   validates_format_of :email_address, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   validates :genres, presence: true, if: :artist
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  # validations for avatar use
+  # has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  # validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   # ratings_as_user means that you are the user giving ratings 
   # on reviews, comments, or artists
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141122194806) do
+ActiveRecord::Schema.define(version: 20141123234303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,9 +63,13 @@ ActiveRecord::Schema.define(version: 20141122194806) do
     t.string   "email_address"
     t.text     "bio"
     t.string   "soundcloud_profile"
-    t.boolean  "artist",             default: false
+    t.boolean  "artist",              default: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
